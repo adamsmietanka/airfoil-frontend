47ed128ec1ff1492287e72c76c9b1f6453f3fa1b
adamsmietanka <adamsmietanka@outlook.com>
2021-05-23 18:38:18 +0200

    feat: move field related logic to a separate component

---
+++ src/components/NumberField.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="field">
+    <label :for="id">{{ label }}</label>
+    <div class="input-append">
+      <input
+        id="id"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+      <div class="unit">{{ unit }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NumberField",
+  props: {
+    id: String,
+    label: String,
+    modelValue: Number,
+    unit: String,
+  },
+  emits: ["update:modelValue"],
+};
+</script>
+
+<style scoped>
+.field {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0.5rem;
+}
+label {
+  margin: 0.25rem 0;
+  font-weight: 700;
+  text-align: left;
+}
+.input-append {
+  position: relative;
+  height: 2rem;
+}
+input {
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  width: 100%;
+  height: 100%;
+  font-size: 1rem;
+  padding-left: 0.5rem;
+}
+input:focus {
+  border-width: 1px;
+}
+.unit {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 3rem;
+  height: 100%;
+  border: 1px solid #ddd;
+  border-radius: 0 4px 4px 0;
+  background: #eee;
+  font-weight: 900;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,30 @@
 <template>
   <div class="form">
-    <label for="span">Span</label>
-    <input id="span" v-model="span" />
-    <label for="chord-fuse">Fuselage chord</label>
-    <input id="chord-fuse" v-model="chord_fuse" />
-    <div class="field">
-      <label for="chord-tip">Tip Chord</label>
-      <div class="input-append">
-        <input id="chord-tip" v-model="chord_tip" />
-        <div class="unit">m</div>
-      </div>
-    </div>
-    <div class="field">
-      <label for="angle">Angle</label>
-      <div class="input-append">
-        <input id="angle" v-model="angle" />
-        <div class="unit">º</div>
-      </div>
-    </div>
+    <NumberField id="span" label="Span" v-model="span" unit="m" />
+    <NumberField
+      id="chord-fuse"
+      label="Fuselage chord"
+      v-model="chord_fuse"
+      unit="m"
+    />
+    <NumberField
+      id="chord-tip"
+      label="Tip Chord"
+      v-model="chord_tip"
+      unit="m"
+    />
+    <NumberField id="angle" label="Angle" v-model="angle" unit="º" />
   </div>
   <div id="wing-plot"></div>
 </template>
 
 <script>
 import Plotly from "plotly.js-gl3d-dist-min";
+import NumberField from "@/components/NumberField";
 
 export default {
   name: "Home",
-  components: {},
+  components: { NumberField },
   data() {
     return {
       span: 10,
@@ -98,41 +95,5 @@ export default {
   display: flex;
   flex-direction: column;
   max-width: 300px;
-}
-.form .field {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 0.5rem;
-}
-label {
-  margin: 0.25rem 0;
-  font-weight: 700;
-  text-align: left;
-}
-.input-append {
-  position: relative;
-  height: 2rem;
-}
-input {
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  width: 100%;
-  height: 100%;
-  font-size: 1rem;
-  padding-left: 0.5rem;
-}
-.unit {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 3rem;
-  height: 100%;
-  border: 1px solid #ddd;
-  border-radius: 0 5px 5px 0;
-  background: #eee;
-  font-weight: 900;
 }
 </style>
